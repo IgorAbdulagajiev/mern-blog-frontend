2578fab8993c74071c2412dfe06bc9be03569338
Igor Abdulagadjiev <s89183906500@gmail.com>
2022-07-01 16:19:11 +0300

    сделал фронт

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+export const Header = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+
+  const onClickLogout = () => {
+    dispatch(logout());
+    window.localStorage.removeItem('token');
+  };
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/add-post">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button
+                  onClick={onClickLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
---
+++ src/redux/slices/posts.js
@@ -0,0 +1,68 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const { data } = await axios.get('/posts');
+  return data;
+});
+
+export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
+  const { data } = await axios.get('/tags');
+  return data;
+});
+
+export const fetchRemovePost = createAsyncThunk("posts/fetchRemovePost", async (id) => {
+  const { data } = await axios.delete(`/posts/${id}`);
+  return data;
+});
+
+const initialState = {
+  posts: {
+    items: [],
+    status: 'loading',
+  },
+  tags: {
+    items: [],
+    status: 'loading',
+  },
+};
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    // Получение статей
+    [fetchPosts.pending]: (state) => {
+      state.posts.items = [];
+      state.posts.status = "loading";
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.posts.status = "loaded";
+      state.posts.items = action.payload;
+    },
+    [fetchPosts.rejected]: (state) => {
+      state.posts.status = "error";
+      state.posts.items = [];
+    },
+    // Получение тегов
+    [fetchTags.pending]: (state) => {
+      state.tags.items = [];
+      state.tags.status = "loading";
+    },
+    [fetchTags.fulfilled]: (state, action) => {
+      state.tags.status = "loaded";
+      state.tags.items = action.payload;
+    },
+    [fetchTags.rejected]: (state) => {
+      state.tags.status = "error";
+      state.tags.tags = [];
+    },
+    // Удаление статей
+    [fetchRemovePost.pending]: (state, action) => {
+      state.posts.items = state.posts.items.filter(obj => obj._id !== action.meta.arg)
+    },
+  },
+});
+
+export const postsReducer = postsSlice.reducer;
